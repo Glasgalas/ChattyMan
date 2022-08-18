e78858e18507fd743139cdca8817230127076fae
Glasgalas <alex.starichenko@icloud.com>
2022-08-18 23:56:25 +0300

    fix search,spinner, add logo

--- src/index.js
+++ src/index.js
@@ -5,21 +5,15 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-// import reportWebVitals from './reportWebVitals';
 import 'modern-normalize/modern-normalize.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    {/* <React.StrictMode> */}
-    <Provider store={store}>
-      <App />
-    </Provider>
-    {/* </React.StrictMode> */}
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
   </BrowserRouter>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
